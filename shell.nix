35255773b6cd5fc3a29d691d3467f541125a888b
Yaroslav Malykh <fisuri@murena.io>
2025-09-07 19:09:57 +0400

    feat: remove LD_LIBRARY_PATH

--- shell.nix
+++ shell.nix
@@ -1,23 +1,14 @@
-{pkgs, ...}: let
-  libPath = with pkgs;
-    lib.makeLibraryPath [
-      libGL
-      libxkbcommon
-      wayland
-    ];
-in
-  pkgs.mkShell {
-    nativeBuildInputs = with pkgs; [
-      cmake
-      qt6.qtbase
-      qt6.qtquick3d
-      qt6.wrapQtAppsHook
-    ];
+{pkgs, ...}:
+pkgs.mkShell {
+  nativeBuildInputs = with pkgs; [
+    cmake
+    qt6.qtbase
+    qt6.qtquick3d
+    qt6.wrapQtAppsHook
+  ];
 
-    buildInputs = with pkgs; [
-      qt6.qtbase
-      qt6.qtquick3d
-    ];
-
-    LD_LIBRARY_PATH = libPath;
-  }
+  buildInputs = with pkgs; [
+    qt6.qtbase
+    qt6.qtquick3d
+  ];
+}
